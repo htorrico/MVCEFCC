72d5019ae5082152e34fb041eede377ef7e5d343
htorrico <htorrico@truenorthcorporation.com>
2025-11-08 09:14:26 -0500

    Add project files.

---
+++ MVCEFCC/Models/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MVCEFCC.Models
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+        }
+        public DbSet<Person> People { get; set; }
+
+        //public DbSet<Customer> Customers { get; set; }
+
+        //public DbSet<Product> Products { get; set; }
+
+
+    }
+}
---
+++ MVCEFCC/Models/Person.cs
@@ -0,0 +1,12 @@
+﻿namespace MVCEFCC.Models
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Phone { get; set; }
+
+    }
+}
